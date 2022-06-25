dba76e76b202bd9302aee59f63474ef8a41809d8
Alfredo Menezes <amtrosul@hotmail.com>
2022-06-24 21:13:03 -0300

    refactor of the newapi lib

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,14 @@ mod theme;
 
 use std::error::Error;
 use dotenv::dotenv;
+use newsapi::{NewsAPI, Endpoint, Article};
 
-use newsapi::{get_articles, Articles};
-
-fn render_articles(articles: &Articles) {
+fn render_articles(articles: &Vec<Article>) {
     let theme = theme::default();
     theme.print_text("# Top headlines\n\n");
-    for i in &articles.articles {
-        theme.print_text(&format!("`{}`", i.title));
-        theme.print_text(&format!("> *{}*", i.url));
+    for i in articles {
+        theme.print_text(&format!("`{}`", i.title()));
+        theme.print_text(&format!("> *{}*", i.url()));
         theme.print_text("---");
     }
 }
@@ -19,13 +18,13 @@ fn render_articles(articles: &Articles) {
 fn main() -> Result<(), Box<dyn Error>> {
     dotenv()?;
     let api_key = std::env::var("API_KEY")?;
-    let url = "https://newsapi.org/v2/top-headlines?country=ca&apiKey=";
 
-    let url = format!("{}{}", url, api_key);
+    let mut newsapi = NewsAPI::new(&api_key);
+    newsapi.endpoint(Endpoint::TopHeadlines);
 
-    let articles = get_articles(&url)?;
+    let newsapi_response = newsapi.fetch()?;
 
-    render_articles(&articles);
+    render_articles(&newsapi_response.articles());
 
     Ok(())
 }
